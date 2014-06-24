532e86e0ec4a3b5602c68033eadf121733d286e9
pinonpierre <pinon.pierre@gmail.com>
2014-06-24 13:56:38 +0200

    - Clean pom.xml - Add missign dependency - Add web.xml jersey definition - Add dropzone JS library

--- dropbitz/src/main/java/org/teknux/dropbitz/rest/FileUpload.java
+++ dropbitz/src/main/java/org/teknux/dropbitz/rest/FileUpload.java
@@ -1,34 +1,36 @@
-package org.teknux.dropbitz;
+package org.teknux.dropbitz.rest;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
 
-
+@Path("upload")
 public class FileUpload {
 
 	@POST
+	@Path("file")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
     public Response uploadFile(
             @FormDataParam("file") final InputStream fileInputStream,
             @FormDataParam("file") final FormDataContentDisposition contentDispositionHeader) {
-
         try {
-            java.nio.file.Path outputPath = FileSystems.getDefault().getPath("/Users/oeil/", "uploadfile");
+        	java.nio.file.Path outputPath = FileSystems.getDefault().getPath("/home/pp/Desktop", "TOTO-" + contentDispositionHeader.getFileName());
             Files.copy(fileInputStream, outputPath);
         } catch (IOException e) {
             e.printStackTrace();
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode()).build();
         }
 
-
         return Response.status(Response.Status.OK.getStatusCode()).build();
-
     }
 }
--- dropbitz/src/main/webapp/index.jsp
+++ dropbitz/src/main/webapp/index.jsp
@@ -1,1 +1,38 @@
-test
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Dropbitz</title>
+
+        <script src="/lib/dropzone.js"></script>
+    </head>
+    <body>
+        <div class="container">
+			Hey
+			
+			<form action="/rest/upload/file" class="dropzone" id="my-awesome-dropzone" enctype="multipart/form-data">
+				<div class="fallback">
+    				<input name="file" type="file" multiple />
+  				</div>
+  			</form>
+    	</div>
+    </body>
+    <script type="text/javascript">
+    	Dropzone.options.myAwesomeDropzone = {
+    	      init: function() {
+    		    this.on("sending", function(file, xhr, formData) {
+        		    alert(file.size);
+    		    	  formData.append("filesize", file.size); // Will send the filesize along with the file as POST data.
+    	    	});
+    		  },
+    		  paramName: "file", // The name that will be used to transfer the file
+    		  maxFilesize: 20000, // MB
+    		  accept: function(file, done) {
+    		    if (file.name == "justinbieber.jpg") {
+    		      done("Naha, you don't.");
+    		    }
+    		    else { done(); }
+    		  }
+    		};
+        </script>
+</html>
