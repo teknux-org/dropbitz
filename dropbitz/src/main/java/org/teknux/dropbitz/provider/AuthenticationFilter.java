b8dc5e82b3e6bc491aee56b0d61b5582763387ea
oeil <perso.feyl@gmail.com>
2014-06-25 11:26:42 +0200

    improve authentication filter

--- dropbitz/src/main/java/org/teknux/dropbitz/provider/AuthenticationFilter.java
+++ dropbitz/src/main/java/org/teknux/dropbitz/provider/AuthenticationFilter.java
@@ -3,9 +3,12 @@ package org.teknux.dropbitz.provider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 
@@ -16,11 +19,25 @@ import java.io.IOException;
 @Authenticated
 public class AuthenticationFilter implements ContainerRequestFilter {
 
+    public static final String SESSION_ATTRIBUTE_SECURE_ID = "DB_SECURE_ID";
+
     private static Logger logger = LoggerFactory.getLogger(AuthenticationFilter.class);
+
+    @Context
+    private HttpServletRequest request;
 
     @Override
     public void filter(ContainerRequestContext containerRequestContext) throws IOException {
         logger.info("Filter Request");
-        throw new WebApplicationException(Response.Status.FORBIDDEN);
+
+        if (request != null) {
+            HttpSession session = request.getSession();
+            String secureId = (String) session.getAttribute(SESSION_ATTRIBUTE_SECURE_ID);
+            if (secureId == null || secureId.isEmpty()) {
+                //TODO: implements secure id comparaison
+                throw new WebApplicationException(Response.Status.FORBIDDEN);
+            }
+
+        }
     }
 }
