3274b0448ac1ade52723ce5f2f9fdadaacd3fcca
pinonpierre <pinon.pierre@gmail.com>
2014-06-28 22:15:10 +0200

    - Improve BeansWrapperHelper : It take class as argument and return reference instead of return TemplateHashModel object (staticModels method)

--- dropbitz/src/main/java/org/teknux/dropbitz/helper/BeansWrapperHelper.java
+++ dropbitz/src/main/java/org/teknux/dropbitz/helper/BeansWrapperHelper.java
@@ -2,16 +2,22 @@ package org.teknux.dropbitz.helper;
 
 import java.util.List;
 import freemarker.ext.beans.BeansWrapper;
+import freemarker.template.SimpleScalar;
 import freemarker.template.TemplateMethodModelEx;
 import freemarker.template.TemplateModelException;
 
 public class BeansWrapperHelper implements TemplateMethodModelEx {
 	@Override
 	public Object exec(@SuppressWarnings("rawtypes") List arguments) throws TemplateModelException {
-		if (arguments.size() != 0) {
-			throw new TemplateModelException("Wrong arguments");
+		if (arguments.size() != 1) {
+			throw new TemplateModelException("Please specify class");
 		}
-
-		return BeansWrapper.getDefaultInstance().getStaticModels();
+		if (arguments.get(0).getClass() != SimpleScalar.class) {
+			throw new TemplateModelException("Bad argument type");
+		}
+		
+		String className = ((SimpleScalar) arguments.get(0)).getAsString();
+		
+		return BeansWrapper.getDefaultInstance().getStaticModels().get(className);
 	}
 }
--- dropbitz/src/main/webapp/views/main/auth.ftl
+++ dropbitz/src/main/webapp/views/main/auth.ftl
@@ -7,7 +7,7 @@
 	
 	<div class="col-lg-12 text-center">
 		<div id="auth-title">
-			<h1>${statics()["org.teknux.dropbitz.Application"].getConfigurationFile().getTitle()}</h1>
+			<h1>${statics("org.teknux.dropbitz.Application").getConfigurationFile().getTitle()}</h1>
 			<span id="landing-page-lead">
 				- Enter your Secure ID to start sharing files -
 			</span>
