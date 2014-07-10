086ca2202fcad58543fd354927d514f3ab559652
oeil <perso.feyl@gmail.com>
2014-07-10 19:54:20 +0200

    moved admin skeleton page

--- dropbitz/src/main/java/org/teknux/dropbitz/freemarker/View.java
+++ dropbitz/src/main/java/org/teknux/dropbitz/freemarker/View.java
@@ -3,7 +3,7 @@ package org.teknux.dropbitz.freemarker;
 public enum View {
     AUTH("/main/auth"),
     DROP("/main/drop"),
-    ADMIN("/main/admin"),
+    ADMIN("/admin/admin"),
     FALLBACK("/upload/fallback");
     
     private String templateName;
